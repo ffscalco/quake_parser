55accacc15f696ce0063ba90563b01d933449e91
Fabiano F. Scalco <fabiano.scalco@gmail.com>
2015-04-28 20:16:28 -0300

    Criado testes para a classe player.

---
+++ spec/player_spec.rb
@@ -0,0 +1,23 @@
+require 'spec_helper'
+require './lib/player'
+
+RSpec.describe Player do
+  let(:player) {Player.new(1)}
+
+  describe "#initialize" do
+    it "should initialize id with param value" do
+      expect(player.instance_variable_get(:@id)).to eq(1)
+    end
+
+    it "should initialize kills with zero" do
+      expect(player.instance_variable_get(:@kills)).to eq(0)
+    end
+  end
+
+  describe "#to_s" do
+    it "should return de player's name" do
+      player.name = "Some Name"
+      expect(player.to_s).to eq("Some Name")
+    end
+  end
+end
