8b24f4a8ebce4fde26e79fe6775c5020cd2b36e3
Fabiano F. Scalco <fabiano.scalco@gmail.com>
2015-04-28 19:29:17 -0300

    Criado funções para buscar um jogador dentro do jogo.

--- lib/game.rb
+++ lib/game.rb
@@ -8,4 +8,21 @@ class Game
     @total_kills = 0
     @players = []
   end
+
+  def find_player(line, type)
+    return self.players.select{|p| p.id == get_id(line, type)}.first
+  end
+
+  private
+    def get_id(line, type)
+      case(type)
+      when "ClientUserinfoChanged"
+        return line.split("ClientUserinfoChanged: ").last[0]
+      when "Kill: 1022"
+        return line.split("Kill: 1022 ").last[0]
+      when "Kill"
+        return line.split("Kill: ").last[0]
+      else
+        return ""
+    end
 end
