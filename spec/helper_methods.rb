48b9aa78ff21a476f23a70c932146a164e647e3d
Fabiano F. Scalco <fabiano.scalco@gmail.com>
2015-05-01 04:09:01 -0300

    Criado novo helper com mais dados para o teste.

--- spec/helper_methods.rb
+++ spec/helper_methods.rb
@@ -14,3 +14,51 @@ def simple_game
 
   game
 end
+
+def medium_game
+  games = []
+
+  game = Game.new("game_1")
+  game.total_kills = 5
+  player1 = Player.new(1)
+  player2 = Player.new(3)
+  player3 = Player.new(5)
+  player1.name = "Zeh"
+  player1.kills = -2
+  player1.total_deaths = 2
+  player1.total_kills = 0
+  player2.name = "Dono da Bola"
+  player2.kills = -1
+  player2.total_deaths = 1
+  player2.total_kills = 1
+  player3.name = "Isgalamido"
+  player3.kills = 1
+  player3.total_deaths = 0
+  player3.total_kills = 1
+  game.players = [player1, player2, player3]
+
+  games << game
+
+  game = Game.new("game_2")
+  game.total_kills = 6
+  player1 = Player.new(1)
+  player2 = Player.new(3)
+  player3 = Player.new(5)
+  player1.name = "Mal"
+  player1.kills = 3
+  player1.total_deaths = 0
+  player1.total_kills = 3
+  player2.name = "Dono da Bola"
+  player2.kills = 1
+  player2.total_deaths = 1
+  player2.total_kills = 0
+  player3.name = "Zeh"
+  player3.kills = 0
+  player3.total_deaths = 1
+  player3.total_kills = 1
+  game.players = [player1, player2, player3]
+
+  games << game
+
+  games
+end
