57dde20e5ead2a74ed061da9597f0ce0714a12f6
Fabiano F. Scalco <fabiano.scalco@gmail.com>
2015-05-01 04:23:13 -0300

    Alterado forma de apresentar os dados finais.

--- quake_parser.rb
+++ quake_parser.rb
@@ -2,4 +2,26 @@
 require './lib/task'
 
 task = Task.new
-ap task.resume_game
+
+ap "Olá, pronto para ver o resultado do log?"
+
+ap "Selecione uma das opções abaixo!"
+ap "Resumo dos jogos: 1"
+ap "Resumo dos jogos com rank dos jogadores: 2"
+ap "Resumo das mortes agrupadas: 3"
+
+option = STDIN.gets
+
+case(option.chomp())
+  when "1"
+    ap task.resume_game
+  when "2"
+    ap "Resumo:"
+    ap task.resume_game
+    ap "Rank:"
+    ap task.rank_game
+  when "3"
+end
+
+ap "Espero que tenham gostado ;]"
+ap "Abraços!"
