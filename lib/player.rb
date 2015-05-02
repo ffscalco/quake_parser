049adc56778db649f3cb9e501daa09a38574cf55
Fabiano F. Scalco <fabiano.scalco@gmail.com>
2015-05-02 02:12:00 -0300

    Criado novo atributo 'online'. Criado função para saber se o player está online ou não.

--- lib/player.rb
+++ lib/player.rb
@@ -1,16 +1,21 @@
 #!/usr/bin/env ruby
 
 class Player
-  attr_accessor :id, :name, :kills, :total_kills, :total_deaths
+  attr_accessor :id, :name, :kills, :total_kills, :total_deaths, :online
 
   def initialize(id)
     @id = id
     @kills = 0
     @total_kills = 0
     @total_deaths = 0
+    @online = true
   end
 
   def to_s
     self.name
+  end
+
+  def online?
+    self.online
   end
 end
--- spec/lib/player_spec.rb
+++ spec/lib/player_spec.rb
@@ -20,12 +20,27 @@ RSpec.describe Player do
     it "should initialize total_deaths with zero" do
       expect(player.instance_variable_get(:@total_deaths)).to eq(0)
     end
+
+    it "should initialize online with true" do
+      expect(player.instance_variable_get(:@online)).to eq(true)
+    end
   end
 
   describe "#to_s" do
     it "should return de player's name" do
       player.name = "Some Name"
       expect(player.to_s).to eq("Some Name")
+    end
+  end
+
+  describe "#online?" do
+    it "should return true if player is online" do
+      expect(player.online?).to eq(true)
+    end
+
+    it "should return false if player is offline" do
+      player.online = false
+      expect(player.online?).to eq(false)
     end
   end
 end
